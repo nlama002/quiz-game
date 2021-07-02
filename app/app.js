ed7a3a879ce2e7854d64b931ea6806ed3c4f0dc7
nlama002 <nlama002@citymail.cuny.edu>
2021-07-02 19:34:06 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,11 @@
+//console.log("nima")
+
+const quizCached = document.getElementById("#quiz")
+const resultCached = document.getElementById("#results")
+const submitCached = document.getElementById("#submit")
+
+
+function quizLogic()
+
+function theResult()
+
