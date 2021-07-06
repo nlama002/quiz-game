49a76b0dc17d43dbbf10fffa470d02ddeebb1b17
nlama002 <nlama002@citymail.cuny.edu>
2021-07-06 11:29:46 -0400

    some functions

--- app/app.js
+++ app/app.js
@@ -3,9 +3,42 @@
 const quizCached = document.getElementById("#quiz")
 const resultCached = document.getElementById("#results")
 const submitCached = document.getElementById("#submit")
+const endGameCached =  document.getElementById("#EndGame")
 
 
-function quizLogic()
+function quizLogic(){
+    
+}
 
 function theResult()
 
+
+//display quiz right away
+quizLogic();
+
+submitCached.addEventListener('click', theResult)
+
+
+//showing the quiz questions
+
+const quizQuestions = [
+    {
+        question: "What is the capital of Nepal ?",
+        answers: {
+        a: "Kathmandu",
+        b: "Washington",
+        c: "Kolkata"
+             }, rightAnswer: "a"
+    },
+
+    {
+        question: "Which one of them is one of the seven wonders of the world?",
+        answers: {
+            a: "Empire State Bulding",
+            b: "Great wall of China",
+            c: "lala land",
+        }, rightAnswer: "b"
+
+    },
+   
+]
