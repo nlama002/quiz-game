a718490b7788879096818d02becb9cf8360585e2
nlama002 <nlama002@citymail.cuny.edu>
2021-07-07 15:30:08 -0400

    adding questions

--- app/app.js
+++ app/app.js
@@ -11,3 +11,52 @@ function theResult(){}
 function score(){}
 
 submitBtn.addEventListener('click', theResult);
+
+const quizQuestions = [
+    {
+        question: "What is the capital of Nepal ?",
+        answers: {
+        a: "Kathmandu",
+        b: "Washington",
+        c: "Kolkata"
+             }, rightAnswer: "a"
+    },
+
+    {
+        question: "Which one of them is one of the seven wonders of the world?",
+        answers: {
+            a: "Empire State Bulding",
+            b: "Great wall of China",
+            c: "lala land",
+        }, rightAnswer: "b"
+
+    },
+    
+    {
+        question: "Which country has won the most number of world cups ?",
+        answers: {
+            a: "Brazil",
+            b: "Germany",
+            c: "Italy"
+
+        }, rightAnswer: "a"
+
+    },
+
+    {
+        question: "Which team has won the most nba titles upto this date?",
+        answers:{
+            a: "Lakers",
+            b: "San Antonio Spurs",
+            c: "Golden State Warriors"
+        }, rightAnswer: "a"
+    },
+    {
+            question: "How many oceans are there in this planet?",
+        answers:{
+        a: "6",
+        b: "4",
+        c: "5"
+        }, rightAnswer: "c",
+    },
+]
