395a5cce986ab8103415a66b5dd0bbcc777e48f8
nlama002 <nlama002@citymail.cuny.edu>
2021-07-07 15:22:23 -0400

    adding functionalities strucutre

--- app/app.js
+++ app/app.js
@@ -4,3 +4,10 @@ const quizCached = document.getElementById('quiz')
 const resultCached = document.getElementById('result')
 const submitBtn = document.getElementById('submit')
 
+
+function quizLogic(){}
+function theResult(){}
+
+function score(){}
+
+submitBtn.addEventListener('click', theResult);
