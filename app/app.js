e36a7560e31a47c47347e719f91b3de124ee6ed1
nlama002 <nlama002@citymail.cuny.edu>
2021-07-07 15:02:44 -0400

    adding buttons with functions

--- app/app.js
+++ app/app.js
@@ -1,51 +1,92 @@
 //console.log("nima")
 
-const quizCached = document.getElementById("#quiz")
-const resultCached = document.getElementById("#results")
-const submitCached = document.getElementById("#submit")
-const endGameCached =  document.getElementById("#EndGame")
-
-
-function quizLogic(){
-    const output = [];
-    quizQuestions.forEach((quest, idx) => 
-    {
-        const ans = [];
-        for (option in quest.answers) {
-            ans.push(
-                `<label>
-                <input type = "radio" name="question${idx}" value = "${option}"> ${option}:
-                ${quest.answers[option]}
-                </label>`
-
-            )
-
+(function(){
+    // Functions
+    function quizLogic(){
+      const output = [];
+      questions.forEach(
+        (currentQuestion, questionIdx) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(ans in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+          answers.push(
+              `<label>
+                <input type="radio" name="question${questionIdx}" value="${ans}">
+                ${ans} :
+                ${currentQuestion.answers[ans]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
         }
-        //adding the question and answer to the output
-        output.push(
-            `<div class = "question"> ${quest.question} </div>
-            <div class = "answers">${ans.join('')} </div>`
-        );
-    })
-        quizCached.innerHTML = output.join('');
-
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      grid.innerHTML = output.join('');
     }
-
-function theResult(){
-
-}
-
-
-//display quiz right away
-quizLogic();
-
-submitCached.addEventListener('click', theResult)
-
-
-//showing the quiz questions
-
-const quizQuestions = [
-    {
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = grid.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      questions.forEach( (currentQuestion, questionIdx) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionIdx];
+        const selector = `input[name=question${questionIdx}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionIdx].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionIdx].style.color = 'red';
+        }
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+    }
+  
+    
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    } 
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    // Variables
+    const grid = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const questions = [
+      {
         question: "What is the capital of Nepal ?",
         answers: {
         a: "Kathmandu",
@@ -84,4 +125,23 @@ const quizQuestions = [
         }, rightAnswer: "a"
     }
    
-]
+
+    ];
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    // Show the first slide
+    showSlide(currentSlide);
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  
+    quizLogic();
+
+    
+  })();
+  
---
+++ app/app1.js
@@ -0,0 +1,25 @@
+const myQuestions = [
+    {
+        question: "what is the capital of Nepal?" ,
+        answers:{
+        a: kathmandu,
+        b: london,
+        c: Newyork,
+        }, rightAnswer: "a"
+    },
+
+    {
+        question: "Whats the year right now ?",
+        anwers:{
+            a: 2021, 
+            b: 2022,
+            c: 2020
+        }, rightAnswer: "a"
+    }
+
+]
+
+myQuestions.forEach((current, idx) =>{
+    console.log("look")
+
+})
