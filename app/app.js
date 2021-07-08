f72e1445d88814d092b5034b3c84df47c537ff42
nlama002 <nlama002@citymail.cuny.edu>
2021-07-08 13:25:47 -0400

    almost done

--- app/app.js
+++ app/app.js
@@ -1,156 +1,178 @@
-//storing html elements into variables 
-
-const quizCached = document.getElementById('quiz')
-const resultCached = document.getElementById('result')
-const submitBtn = document.getElementById('submit')
-const previousBtn = document.getElementsById('previous')
-const nextBtn =  document.getElementsById('next')
-const slideCached = document.querySelectorAll('.slide')
-
-function quizLogic(){}
-function theResult(){}
-
-function score(){}
-
-submitBtn.addEventListener('click', theResult);
-
-const quizQuestions = [
-    {
-        question: "What is the capital of Nepal ?",
-        answers: {
-        a: "Kathmandu",
-        b: "Washington",
-        c: "Kolkata"
-             }, rightAnswer: "a"
-    },
-
-    {
-        question: "Which one of them is one of the seven wonders of the world?",
-        answers: {
-            a: "Empire State Bulding",
-            b: "Great wall of China",
-            c: "lala land",
-        }, rightAnswer: "b"
-
-    },
-    
-    {
-        question: "Which country has won the most number of world cups ?",
-        answers: {
-            a: "Brazil",
-            b: "Germany",
-            c: "Italy"
-
-        }, rightAnswer: "a"
-
-    },
-
-    {
-        question: "Which team has won the most nba titles upto this date?",
-        answers:{
-            a: "Lakers",
-            b: "San Antonio Spurs",
-            c: "Golden State Warriors"
-        }, rightAnswer: "a"
-    },
-    {
-            question: "How many oceans are there in this planet?",
-        answers:{
-        a: "6",
-        b: "4",
-        c: "5"
-        }, rightAnswer: "c",
-    },
-]
-
-function quizLogic(){
-
-    const output = []
-    // checking each questions and showing their output
-
-    quizQuestions.forEach((element,idx) => {
-        const answers = [];
-        // here we store the list of answers
-        for (theAns in element.answers){
+(function(){
+    function quizLogic(){
+      const output = [];
+  
+          // Checking each questions and showing their output 
+    quizQuestions.forEach(
+        (element, idx) => {
+  
+          const answers = [];
+  
+          // storing list of answers
+          for(theAns in element.answers){
+  
+            // here the radio button was added so that the user could select the answers
             answers.push(
-                `<label>
-                 <input type ='radio' name='question${idx}' value='${theAns}'>${theAns}:
-                ${element.answer[theAns]}
-                </label>`
+              `<label>
+                <input type="radio" name="question${idx}" value="${theAns}">
+                ${theAns} :
+                ${element.answers[theAns]}
+              </label>`
             );
-        }
-        
-        output.push(
-            `<div class = "slide">
-                <div class='question'> ${element.question}</div>
-                <div class = 'answers'>${answers.join('')}</div>
+          }
+  
+          // add this question and its answers to the output
+          // here also the class slide is created so that we can slide questions and answers in display
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${element.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
             </div>`
-        )
-})
-        quizCached.innerHTML = output.join('')
-
-    
-}
-
-function theResult(){
-    const allAnswers = quizCached.querySelectorAll('.answers')
-
-    //tracking the users answers and initializing to 0
-    let totCorrect = 0;
-
-    quizQuestions.forEach((element,idx)=>{
-        // to get the selected answer from user // idx here is question number
-        const eachAnswer = allAnswers[idx]
-
-        // Css selector to check which radio button was checked
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizCached.innerHTML = output.join('');
+    }
+  
+    function theResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizCached.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      quizQuestions.forEach( (element, idx) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[idx];
         const selector = `input[name=question${idx}]:checked`;
-        const theUserAnswer = (eachAnswer.querySelector(selector) || {}).value
-
-
-        //to check if the answer is correct 
-
-        if (theUserAnswer === element.rightAnswer){
-            totCorrect++
-            allAnswers[idx].style.color = 'green'
-
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === element.rightAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[idx].style.color = 'green';
         }
+        // if answer is wrong or blank
         else{
-            allAnswers[idx].style.color ='red'
+          // color the answers red
+          answerContainers[idx].style.color = 'red';
         }
-
-
-    })
-    resultCached.innerHTML = `${totCorrect} out of ${quizQuestions.length}`
-
-}
-
-let presentSlide = 0;
-function slideClass(a){
-    // hiding the present slide by removing the active slide
-
-    slideCached[presentSlide].classList.remove('active-slide')
-    //showing the new slide by adding the active slide
-    slideCached[a].classList.add('active-slide')
-    //updating the slide number
-    presentSlide = a
-
-    //this condition hides the previous slide button if we onto
-    //next slide button
-    if (presentSlide === 0){
-        previousBtn.style.display = 'none'
+      });
+  
+      // show number of correct answers out of total
+      resultCached.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+    //   if (numCorrect >=3){
+    //       scoreCached.innerHTML = console.log("You passed")
+        
+    //   }
+    //   else{
+    //       scoreCached.innerHTML = console.log("You failed")
+    //   }
     }
-    else{
-        previousBtn.style.display= 'inline-block'
-
-    //this slide shows the submit button if we are on the last slide
+  
+    function showSlide(n) {
+      slides[presentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      presentSlide = n;
+      if(presentSlide === 0){
+        previousBtn.style.display = 'none';
+      }
+      else{
+        previousBtn.style.display = 'inline-block';
+      }
+      if(presentSlide === slides.length-1){
+        nextBtn.style.display = 'none';
+        submitBtn.style.display = 'inline-block';
+      }
+      else{
+        nextBtn.style.display = 'inline-block';
+        submitBtn.style.display = 'none';
+      }
     }
-    if (presentSlide === slideCached.length-1){
-        nextBtn.style.display='none'
-        submitBtn.style.display ='inline-block'
+  
+    function showNextSlide() {
+      showSlide(presentSlide + 1);
     }
-    else{
-        nextBtn.style.display ='inline-block'
-        submitBtn.style.display='none'
+  
+    function showPreviousSlide() {
+      showSlide(presentSlide - 1);
     }
+  
+    // Variables for the DOM manupulation
+    const quizCached = document.getElementById('quiz');
+    const resultCached = document.getElementById('results');
+    const submitBtn = document.getElementById('submit');
+    //questions and answers for the game
+    const quizQuestions = [
+        {
+            question: "What is the capital of United States ?",
+            answers: {
+            a: "Kathmandu",
+            b: "Washington",
+            c: "New York"
+                 }, rightAnswer: "a"
+        },
     
-}
+        {
+            question: "Which one of them is one of the seven wonders of the world?",
+            answers: {
+                a: "Empire State Bulding",
+                b: "Great wall of China",
+                c: "lala land",
+            }, rightAnswer: "b"
+    
+        },
+        
+        {
+            question: "Which country has won the most number of world cups ?",
+            answers: {
+                a: "Brazil",
+                b: "Germany",
+                c: "Italy"
+    
+            }, rightAnswer: "a"
+    
+        },
+    
+        {
+            question: "Which team has won the most nba titles upto this date?",
+            answers:{
+                a: "Lakers",
+                b: "San Antonio Spurs",
+                c: "Golden State Warriors"
+            }, rightAnswer: "a"
+        },
+        {
+                question: "How many oceans are there in this planet?",
+            answers:{
+            a: "6",
+            b: "4",
+            c: "5"
+            }, rightAnswer: "c",
+        },
+        
+    ]
+    
+    quizLogic();
+    const previousBtn = document.getElementById("previous");
+    const nextBtn = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let presentSlide = 0;
+  
+    // Show the first slide
+    showSlide(presentSlide);
+  
+    // Event listeners
+    submitBtn.addEventListener('click', theResults);
+    previousBtn.addEventListener("click", showPreviousSlide);
+    nextBtn.addEventListener("click", showNextSlide);
+  })();
+  
---
+++ readme.md
@@ -0,0 +1,9 @@
+# General Knowledge Quiz
+The game that we will be playing today is a quiz game. There are 5 different questions for now and the answer selected will turn green if it is correct and red if it is wrong. 
+![Getting Started](/Users/nimatsheringlama/Desktop/Screen Shot 2021-07-08 at 1.13.17 PM.png)
+
+The link to my deployed game is: 
+
+The technology used to build this game was html, css and javascript.
+
+For more future enhancement I will be adding sounds when the answer is correct and when the answer is wrong. I also need to ad
